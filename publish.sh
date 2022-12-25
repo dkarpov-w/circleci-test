1f5f1f26c6e2b0a4a0e249ff8dfb0a5b439b4e27
CircleCI deploy <CircleCI@mydomain.com>
2022-12-25 17:54:12 +0200

    Change: add more details info about commit mesage

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # exit when any command fails
 set -e
 
@@ -8,15 +9,28 @@ PACKAGE_VERSION=$(cat package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
 
-# latest commit message
-COMMIT_MESSAGE=$(git log -1 --format=%s)
+# commit info
+COMMIT_AUTHOR_NAME=$(git log -1 --format="%an")
+COMMIT_AUTHOR_EMAIL=$(git log -1 --format="%ae")
+COMMIT_SUBJECT=$(git log -1 --format="%s")
+COMMIT_BODY=$(git log -1 --format="%b")
+COMMIT_NOTE=$(git log -1 --format="%N")
+TAG_ANNOTATION="
+$COMMIT_AUTHOR_NAME
+$COMMIT_AUTHOR_EMAIL
+
+$COMMIT_SUBJECT
+$COMMIT_BODY
+$COMMIT_NOTE"
 
 git config user.email "CircleCI@mydomain.com"
 git config user.name "CircleCI deploy"
 
-echo "Package version -> $PACKAGE_VERSION"
-echo "Message -> $COMMIT_MESSAGE"
-git tag $PACKAGE_VERSION -m "$COMMIT_MESSAGE"
+echo "Package version --> $PACKAGE_VERSION"
+echo "-------------"
+echo "Message --> $TAG_ANNOTATION"
+echo "-------------"
+# git tag $PACKAGE_VERSION -m "$COMMIT_MESSAGE"
 
-echo "Publishing version $PACKAGE_VERSION"
-git push origin $PACKAGE_VERSION
+# echo "Publishing version $PACKAGE_VERSION"
+# git push origin $PACKAGE_VERSION

4db5be985cbbe1e253bb56ad4d08f995d8d1759a
CircleCI deploy <CircleCI@mydomain.com>
2022-12-25 18:11:17 +0200

    Change: fix bash

--- publish.sh
+++ publish.sh
@@ -14,9 +14,17 @@ COMMIT_MESSAGE=$(git log -1 --format=%s)
 git config user.email "CircleCI@mydomain.com"
 git config user.name "CircleCI deploy"
 
+<<<<<<< Updated upstream
 echo "Package version -> $PACKAGE_VERSION"
 echo "Message -> $COMMIT_MESSAGE"
 git tag $PACKAGE_VERSION -m "$COMMIT_MESSAGE"
+=======
+echo "Package version --> $PACKAGE_VERSION"
+echo "-------------"
+echo "Message --> $TAG_ANNOTATION"
+echo "-------------"
+git tag $PACKAGE_VERSION -m "$TAG_ANNOTATION"
+>>>>>>> Stashed changes
 
 echo "Publishing version $PACKAGE_VERSION"
 git push origin $PACKAGE_VERSION
